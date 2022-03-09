dd06252249785ff232e71d4571a3632cbb8d92a5
Yosua Vega <yosuavega3gp@gmail.com>
2022-03-10 04:46:49 +0700

    create image and OOP player

--- main.py
+++ main.py
@@ -7,16 +7,30 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Metal Slug")
 
 
+class Soldier(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('assets/img/player/idle/0.png')
+        self.image = pygame.transform.scale(
+            img, (int(img.get_width() * scale), int(img.get_height()*scale)))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+
+player = Soldier(200, 200, 3)
 x = 200
 y = 200
-img = pygame.image.load('img/player/idle/0.png')
-
+scale = 3
 
 run = True
 while run:
+    player.draw()
     for event in pygame.event.get():
-        # quit Game
+        # quit game
         if event.type == pygame.QUIT:
             run = False
-
+    pygame.display.update()
 pygame.quit()
